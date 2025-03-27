a5ac3b094b27113e435bb4afc811faca25a5f9c4
9021517 <9021517@student.zadkine.nl>
2025-03-27 09:36:13 +0100

    Add files via upload

---
+++ crud_fiets/delete.php
@@ -0,0 +1,17 @@
+<?php
+// auteur: zhulide
+// functie: verwijder een bier op basis van de id
+include 'functions.php';
+
+// Haal bier uit de database
+if(isset($_GET['id'])){
+
+    // test of insert gelukt is
+    if(deleteRecord($_GET['id']) == true){
+        echo '<script>alert("Fietscode: ' . $_GET['id'] . ' is verwijderd")</script>';
+        echo "<script> location.replace('index.php'); </script>";
+    } else {
+        echo '<script>alert("Fiets is NIET verwijderd")</script>';
+    }
+}
+?>
